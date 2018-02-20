bde7a33a4f200c015ae5399d3fdf94038382a6ed
William Archimede <william.archimede@hoodbrains.com>
2018-02-20 15:18:26 +0100

    Initial Commit

---
+++ CalaCukePOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalaCukePOC
+//
+//  Created by William Archimède on 20/02/2018.
+//  Copyright © 2018 France Televisions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
